f33f83212631b24a2fbcad5906bb5f1585be1b02
Lenilson-Duarte <lenilson.duarte.etech@gmail.com>
2024-04-05 21:35:57 -0300

    idade

---
+++ idade.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int idade;
+    float altura;
+
+    cout <<"Digite a sua idade: "<<endl;
+    cin >> idade;
+    cout <<"Digite a sua altura: "<<endl;
+    cin >> altura;
+
+    cout << "Idade: " << idade << endl;
+    cout << "Altura: " << altura << endl;
+    return 0;
+}
